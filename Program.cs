9cf2c393c7cdc8a3e0b5431d5e15549bcd5d2cca
Chris Risner <chris@chrisrisner.com>
2015-06-29 10:00:53 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//  using Microsoft;
+
+namespace TestConsole1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
